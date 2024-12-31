60251ea6c2a19b1d10c472e8064e5fd0d4d63420
45akshat <akshatparmar78543@gmail.com>
2024-12-31 20:22:13 +0530

    first commit1111111111111

--- src/models/codes.model.js
+++ src/models/codes.model.js
@@ -23,6 +23,16 @@ const codeSchema = new mongoose.Schema({
   }
 });
 
+// Middleware to adjust Date to IST before saving
+codeSchema.pre('save', function (next) {
+  if (this.Date) {
+    // Convert UTC date to IST (UTC +5:30)
+    const ISTOffset = 5.5 * 60 * 60 * 1000; // 5 hours 30 minutes in milliseconds
+    this.Date = new Date(this.Date.getTime() + ISTOffset);
+  }
+  next();
+});
+
 const Code = mongoose.model('Code', codeSchema, 'codes');
 
 module.exports = Code;
