80b81203b1748cb295e73a41ed303b1a8fc60972
45akshat <akshatparmar78543@gmail.com>
2025-01-18 12:07:30 +0530

    first commit111111111111111111111111111111111111111111

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    url: process.env.MONGODB_URI || 'mongodb+srv://slayde:slayde9638@cluster0.xycoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
+    url: process.env.MONGODB_URI || 'mongodb+srv://slayde:7AQA67B1VeAvhMAp@cluster0.xycoh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0&ssl=true',
     options: {
         useNewUrlParser: true,
         useUnifiedTopology: true,
